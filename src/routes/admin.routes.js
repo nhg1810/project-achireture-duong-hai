c114e32daa0fcbc3e22cb282005f831f967b1304
unknown <caosonhai1410@gmail.com>
2023-02-06 13:27:17 +0700

    hai

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -2,13 +2,10 @@ const router = require("express").Router();
 const AdminController = require("../app/controllers/AdminController");
 
 router.get('/project-manager', AdminController.projectManager);
-
 router.get('/project-personnel-manager', AdminController.projectPersonnelManager);
 router.get('/project-photo-manager', AdminController.projectPhotoManager);
-
-router.post('/store-category-project' , AdminController.createCategoryProject)
+router.post('/store-category-project', AdminController.createCategoryProject)
 router.post('/store-project', AdminController.createProdject);
 router.get('/', AdminController.index);
-
 
 module.exports = router;
