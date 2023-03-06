2bcda231c628533740d57d76e1d3fcb3bc05d18d
= <=>
2023-03-06 10:28:31 +0700

    dp

--- src/app/config/db/index.js
+++ src/app/config/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/test-nodejs');
+        await mongoose.connect('mongodb+srv://nhduong20it7:ULD0Rrp7PkT3QsCp@cluster0.5eke8u3.mongodb.net/?retryWrites=true&w=majority');
         console.log("connect ok !!!");
     } catch (error) {
         console.log("connect failure !!!");
